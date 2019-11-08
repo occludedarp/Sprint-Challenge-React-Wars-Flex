09cd5bc4f9ab3897793163064d276615666e97c4
occludedarp <cmrnmr@gmail.com>
2019-11-08 13:54:10 -0500

    made parnet child components imported react and styled components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Container, Row} from 'reactstrap';
+import styled from 'styled-components';
+import CharacterCard from './Character';
+
+const Header = styled.div`
+  background: #90bdbf;
+  padding-top: 5%;
+  padding-bottom: 5%;
+`
+
+const HeaderTitle = styled.h1`
+  text-align: center;
+  color: whiteSmoke;
+  font-size: 5rem;
+`
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const [starWarsChar, setStarWarsChar] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+useEffect(() => {
+  axios.get('https://swapi.co/api/people/')
+    .then( response => {
+      console.log(response.data)
+      setStarWarsChar(response.data.results)
+    })
+    .catch( error => {
+      console.log( "no data was returned", error)
+    })
+}, [])
 
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
+    <Container>
+      <Header>
+        <HeaderTitle className="Header">React Wars</HeaderTitle>
+      </Header>
+      <Row>
+      {starWarsChar.map((details, index) => {
+        return (
+          <CharacterCard
+            key={index}
+            characterName={details.name}
+            characterHeight={details.height}
+            characterEyeColor={details.eye_color}
+            characterHairColor={details.hair_color}
+            characterBirthdate={details.birth_year}
+          />
+        )
+      })}
+      </Row>
+    </Container>
+    
   );
+
 }
 
 export default App;
